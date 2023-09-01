0828cebc742a5204c3b7e35cff3a536d072f0be1
arthur <fatih@qq.com>
2023-09-01 23:25:38 +0800

    update client

---
+++ src/democlient.cpp
@@ -0,0 +1,27 @@
+#include "tcp_client.h"
+#include "user_session.h"
+#include <chrono>
+#include <thread>
+
+
+
+
+int main(int argc, char *argv[]){
+
+    TcpClient<UserSession> tcpClient; 
+
+
+    auto conn = tcpClient.connect("127.0.0.1", 8888); 
+
+
+
+    tcpClient.start(); 
+    while(1){
+
+        std::this_thread::sleep_for(std::chrono::seconds(1)); 
+        printf("send message\n"); 
+        conn->msend("hello"); 
+
+    }
+
+}
