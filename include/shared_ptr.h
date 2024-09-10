b0221c7d11c5a91ef6f995e302747ec483a4b487
arthur <fatih@qq.com>
2024-09-10 21:06:06 +0800

    update

---
+++ include/shared_ptr.h
@@ -0,0 +1,230 @@
+#pragma once 
+#include <inttypes.h>
+#include <atomic>
+#include <memory>
+template<typename T>
+class WeakPtr; 
+
+// 引用计数类
+template <class T> 
+class RefCount {
+public:
+ 
+    template<class ... Args> 
+    RefCount(Args ... args) : count(0), object(args ...) {}
+
+    void add_ref() {
+        ++count;
+    }
+
+    int release_ref() {
+        return --count;
+    }
+
+    int get_count() const {
+        return count;
+    }
+
+    T & get() {
+        return object; 
+    }
+     const T & get() const {
+        return object; 
+    }
+
+    
+private:  
+    std::atomic<int> count;  
+    T  object; 
+};
+
+
+// 自定义 shared_ptr 类
+template <typename T>
+class SharedPtr {
+public:
+ 
+    template <class ... Args> 
+    SharedPtr(Args ...  args ) : ref_count(new RefCount<T>(args ... )) {  
+        ref_count->add_ref(); 
+    }
+
+ 
+    SharedPtr(const SharedPtr& other) :  ref_count(other.ref_count) {
+        ref_count->add_ref();
+    }
+
+
+    
+    SharedPtr(const WeakPtr<T>& weakPtr)  {
+          ref_count = weakPtr.ref_count;
+        if (ref_count && ref_count->get_count() > 0) { 
+            ref_count->add_ref();
+        } else { 
+            ref_count = nullptr;
+        }
+    }
+ 
+    SharedPtr(SharedPtr&& other) noexcept :   ref_count(other.ref_count) {
+        other.ref_count = nullptr;
+    }
+
+ 
+    SharedPtr& operator=(const SharedPtr& other) {
+        if (this != &other) {
+            release();       
+            ref_count = other.ref_count;
+            ref_count->add_ref();
+        }
+        return *this;
+    }
+
+ 
+    SharedPtr& operator=(SharedPtr&& other) noexcept {
+        if (this != &other) {
+            release();           
+            ref_count = other.ref_count; 
+            other.ref_count = nullptr;
+        }
+        return *this;
+    }
+
+ 
+    ~SharedPtr() {
+        release();
+    }
+
+ 
+    T& operator*() const {
+        return ref_count->object;
+    }
+
+ 
+    T* operator->() const {
+        return &(ref_count->get());
+    }
+
+ 
+    explicit operator bool() const {
+        return ref_count != nullptr;
+    }
+
+ 
+    int use_count() const {
+        return ref_count ? ref_count->get_count() : 0;
+    }
+
+private:  
+    RefCount<T> * ref_count = nullptr;  
+
+    void release() {
+        if (ref_count && ref_count->release_ref() == 0) {    
+            printf("release from sheard ptr %d\n", ref_count->get_count()); 
+            delete ref_count;
+        }
+    }
+};
+
+
+template<typename T>
+class WeakPtr {
+public:
+    WeakPtr() :   ref_count(nullptr) {}
+
+
+    WeakPtr(const SharedPtr<T>& shared_ptr) :  ref_count(shared_ptr.ref_count) {
+        if (ref_count) {
+  
+        }
+    }
+
+    
+    WeakPtr(const WeakPtr& other) :   ref_count(other.ref_count) {       
+    }
+
+ 
+    ~WeakPtr() {
+        release();
+    }
+
+    WeakPtr& operator=(const WeakPtr& other) {
+        if (this != &other) {
+            release();
+ 
+            ref_count = other.ref_count;
+            if (ref_count) {
+             
+            }
+        }
+        return *this;
+    }
+ 
+    SharedPtr<T> lock() const {
+        if (ref_count && ref_count->get_count() > 0) {
+            return SharedPtr<T>(*this);
+        } else {
+            return SharedPtr<T>();
+        }
+    }
+ 
+
+private:
+    RefCount<T> * ref_count = nullptr;  
+
+    void release() {
+        if (ref_count) { 
+            if (ref_count->get_count() == 0  ) {
+                printf("release from weak ptr\n"); 
+                //delete ref_count;
+            }
+        }
+    }
+
+ 
+    friend class SharedPtr<T>;
+};
+
+template <typename T>
+class EnableSharedFromThis {
+public:
+    SharedPtr<T> shared_from_this() {
+        // 检查 weak_ptr 是否为空
+        if (auto sp = weak_this.lock()) {
+            return sp;
+        } else {
+            throw std::bad_weak_ptr();
+        }
+    }
+
+    SharedPtr<const T> shared_from_this() const {
+        if (auto sp = weak_this.lock()) {
+            return sp;
+        } else {
+             throw std::bad_weak_ptr();
+        }
+    }
+
+protected:
+ 
+    EnableSharedFromThis() = default; 
+    EnableSharedFromThis(const EnableSharedFromThis&) = delete;
+    EnableSharedFromThis& operator=(const EnableSharedFromThis&) = delete;
+
+
+    template <typename U>
+    friend class SharedPtr;
+
+private: 
+    mutable WeakPtr<T> weak_this;
+};
+
+
+
+template <class T, class ... Args > 
+   SharedPtr<T>  create_shared(Args ... args){
+        return  SharedPtr<T>(args ...); 
+   }
+
+
+
+
---
+++ src/testptr.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "shared_ptr.h"
+#include <memory>
+
+class StdClass : public std::enable_shared_from_this<StdClass> 
+{
+    public: 
+    ~StdClass()
+    {
+        printf("destroy StdClass \n");
+    }
+    uint64_t index;
+
+}; 
+
+class MyClass : public EnableSharedFromThis<MyClass>
+{
+public:
+    ~MyClass()
+    {
+        printf("destroy MyClass \n");
+    }
+    uint64_t index;
+};
+
+int main(int argc, char *argv[])
+{
+
+     SharedPtr<MyClass> myClass  = create_shared<MyClass>();
+
+    // // printf("shared ptr size %lu\n", sizeof(myClass));
+    // // printf("ref count %d\n", myClass.use_count());
+
+    // // auto ptr2 = myClass;
+
+    // // printf("ref count %d\n", myClass.use_count());
+
+    // auto ptr3 = myClass->shared_from_this();
+
+    // printf("ref count %d\n", myClass.use_count());
+
+
+    std::shared_ptr<StdClass> stdClass = std::make_shared<StdClass>(); 
+    auto ptr4 = stdClass->shared_from_this(); 
+     printf("ref count %d\n", stdClass.use_count());
+}
