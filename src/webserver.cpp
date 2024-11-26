4f06c0d0f7ef9a539b1f4f3091307c9255563c92
arthur <fatih@qq.com>
2024-11-26 21:01:19 +0800

    add webserver

---
+++ src/webserver.cpp
@@ -0,0 +1,44 @@
+
+
+
+#include "http/http_server.h"
+#include <iostream>
+using namespace snet::http;
+
+int main(int argc, char *argv[])
+{
+
+ 	//knet_add_console_sink();
+	HttpServer<> webSrv;
+	//when use auto , don't forget add "&"
+	webSrv.register_router("/", [](const HttpRequest &req, HttpResponse &  rsp)
+						   { return rsp.write("welcome my website"); });
+
+
+	webSrv.register_router("/index", [](const HttpRequest &req, HttpResponse &  rsp) {
+
+		std::cout << "path " << req.path() << std::endl;
+		std::cout << "query " << req.query() << std::endl;
+		std::cout << " req " << req.to_string() << std::endl;
+		return rsp.write("hello server", 200);
+	});
+
+	webSrv.register_router("/context", [](const HttpContextPtr &  ctx)
+						   { return ctx->write("hello server"); });
+	snet::NetOptions netOpts{};
+	//netOpts.sync_accept_threads = 2;
+	webSrv.start(8888, "0.0.0.0", netOpts);
+	char c = getchar();
+	while (c)
+	{
+
+		if (c == 'q')
+		{
+			printf("quiting ...\n");
+			break;
+		}
+		c = getchar();
+	}
+
+	return 0;
+};
